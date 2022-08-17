3736f6b5f430cfe5bac6e98c8d7db79f4ed402bd
chenwangnatsukashii <390827590@qq.com>
2022-08-17 12:10:23 +0800

    Initial commit

---
+++ src/test/java/com/example/optaplanner_industry/OptaplannerIndustryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.optaplanner_industry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptaplannerIndustryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
