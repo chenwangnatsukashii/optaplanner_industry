3a7d67c3d27470dadf3e35cb550a3e5446bf6e4e
chenwangnatsukashii <390827590@qq.com>
2022-09-05 16:35:25 +0800

    添加Json

--- src/main/java/com/example/optaplanner_industry/demo/domain/Step.java
+++ src/main/java/com/example/optaplanner_industry/demo/domain/Step.java
@@ -1,5 +1,12 @@
 package com.example.optaplanner_industry.demo.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Step {
 
     // 步骤ID
